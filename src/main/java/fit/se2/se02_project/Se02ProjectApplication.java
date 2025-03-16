62a5a95c352c56c2c8174de8ef3e52cd7e2ae178
Vu Ngoc Cuong <vncuong2810@gmail.com>
2025-03-16 23:44:35 +0700

    Initial draft

---
+++ src/main/java/fit/se2/se02_project/Se02ProjectApplication.java
@@ -0,0 +1,11 @@
+package fit.se2.se02_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se02ProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Se02ProjectApplication.class, args);
+	}
+}
